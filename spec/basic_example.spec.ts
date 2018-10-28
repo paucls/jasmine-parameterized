3e67d135b5b6340c5b360ebc61c980aa4c2162a3
Paulo Clavijo Esteban <paucls@gmail.com>
2018-10-28 16:45:25 +0000

    Adding basic example of parameterized test with a single param per case.

---
+++ spec/basic_example.spec.ts
@@ -0,0 +1,33 @@
+import { cases } from '../lib';
+
+describe('Customer', () => {
+
+    cases([
+        4,
+        14
+    ])
+    .it('should be a child when aged 4 to 14 years', (age) => {
+        const customer = new Customer(age);
+        expect(customer.isChild()).toBe(true);
+    });
+
+    cases([
+        3,
+        15,
+        18
+    ])
+    .it('should not be an child in other cases', (age) => {
+        const customer = new Customer(age);
+        expect(customer.isChild()).toBe(false);
+    });
+
+});
+
+// Impl
+class Customer {
+    constructor(private age: number) {}
+
+    isChild(): boolean {
+        return this.age >= 4 && this.age <= 14;
+    }
+}
